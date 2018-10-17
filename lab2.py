48a737f6e22e28a1a7c694d82bbb3dbfc24b5fc1
wgalka <37069490+wgalka@users.noreply.github.com>
2018-10-17 10:31:23 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -29,3 +29,28 @@ a = a + 3
 print(a)
 
 # zad2
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return print(a)
+
+
+
+gcd(10, 25)  # => 5
+gcd(14, 15)  # => 1
+gcd(3, 9)  # => 3
+gcd(1, 1)  # => 1
+
+# zad3
+
+my_map = {"CA": "US", "NY": "US", "ON": "CA"}
+inv_map = {value: key for key, value in my_map.items()}
+inv_map = {}
+for key, value in my_map.items():
+    inv_map[value] = inv_map.get(value, [])
+    inv_map[value].append(key)
+
+print(inv_map)
+
+# zad4
+
