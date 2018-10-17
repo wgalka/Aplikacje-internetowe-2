4d9efc03e603c7f350065b90d4cde76de2cf6bbd
wgalka <37069490+wgalka@users.noreply.github.com>
2018-10-17 09:42:05 +0200

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,31 @@
+# zad1 ...
+
+s = [0] * 3
+s[0] += 1
+print(s)
+
+s = [''] * 3
+s[0] += 'a'
+print(s)
+
+s = [[]] * 3
+s[2] += [1]
+print(s)
+
+s = [[]] * 3
+s[0] = s[0] + [1]
+print(s)
+
+s = [[[]]] * 3
+print(s)
+s[0] += [1]
+print(s)
+
+a = 2
+a += 3
+print(a)
+a = 2
+a = a + 3
+print(a)
+
+# zad2
