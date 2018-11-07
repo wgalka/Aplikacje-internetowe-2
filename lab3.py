5e5167d212e3f653f6f16533e1fb7c684c93738d
wgalka <37069490+wgalka@users.noreply.github.com>
2018-11-07 22:50:21 +0100

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -58,4 +58,175 @@ variadic(8, *[3, 4, 5], k=1, **{'a':5, 'b':'x'})
 #variadic(*[3, 4, 5], 8, *(4, 1), k=1, **{'a':5, 'b':'x'}) invalid syntax
 variadic({'a':5, 'b':'x'}, *{'a':5, 'b':'x'}, **{'a':5, 'b':'x'})
 
+#Optional: Putting it all together
+
+def all_together(x, y, z=1, *nums, indent=True, spaces=4, **options):
+    print("x:", x)
+    print("y:", y)
+    print("z:", z)
+    print("nums:", nums)
+    print("indent:", indent)
+    print("spaces:", spaces)
+    print("options:", options)
+
+#all_together(2)  TypeError: all_together() missing 1 required positional argument: 'y'
+all_together(2, 5, 7, 8, indent=False)
+all_together(2, 5, 7, 6, indent=None)
+#all_together()  TypeError: all_together() missing 2 required positional arguments: 'x' and 'y'
+#all_together(indent=True, 3, 4, 5)  SyntaxError: positional argument follows keyword argument
+#all_together(**{'indent': False}, scope='maximum')  TypeError: all_together() missing 2 required positional arguments: 'x' and 'y'
+all_together(dict(x=0, y=1), *range(10))
+#all_together(**dict(x=0, y=1), *range(10))  SyntaxError: iterable argument unpacking follows keyword argument unpacking
+#all_together(*range(10), **dict(x=0, y=1))  TypeError: all_together() got multiple values for argument 'x'
+all_together([1, 2], {3:4})
+#all_together(8, 9, 10, *[2, 4, 6], x=7, spaces=0, **{'a':5, 'b':'x'})  TypeError: all_together() got multiple values for argument 'x'
+all_together(8, 9, 10, *[2, 4, 6], spaces=0, **{'a':[4,5], 'b':'x'})
+all_together(8, 9, *[2, 4, 6], *dict(z=1), spaces=0, **{'a':[4,5], 'b':'x'})
+
+#Writing Functions
+
+def speak_excitedly(message, number=1, capitalize=False):
+    if capitalize == False:
+        print(message, number*'!')
+    else:
+        print(message.upper(), number*'!')
+
+speak_excitedly('I love Python')
+speak_excitedly('Keyword arguments are great',4)
+speak_excitedly('I guess Java is okay...', 0)
+speak_excitedly("LET'S GO STANFORD",2,True)
+
+#average
+
+def average(*nums):
+    if not nums:
+        print(None)
+    else:
+        suma = sum(nums)
+        print(suma/nums.__len__())
+
+average()
+average(5)
+average(6, 8, 9, 11)
+average(*[6, 8, 9, 11])
+
+#Challenge: make_table
+
+def make_table(key_justify='left', value_justify='right', **kwargs):
+
+    if key_justify == 'left':
+        key_justify = '<'
+    elif key_justify == 'right':
+        key_justify = '>'
+    elif key_justify == 'center':
+        key_justify = '^'
+
+    if value_justify == 'left':
+        value_justify = '<'
+    elif value_justify == 'right':
+        value_justify = '>'
+    elif value_justify == 'center':
+        value_justify = '^'
+
+    max_key_length = max(map(len, kwargs.keys()))
+    max_value_length = max(map(len, kwargs.values()))
+
+    total_length = 2 + max_key_length + 3 + max_value_length + 2
+    print('=' * total_length)
+    for key, value in kwargs.items():
+        print('| {:{align1}{length1}} | {:{align2}{length2}} |'.format(key, value,
+                                                                                 align1=key_justify,
+                                                                                 length1=max_key_length,
+                                                                                 align2=value_justify,
+                                                                                 length2=max_value_length
+                                                                                 ))
+    print('=' * total_length)
+
+def test_make_table():
+    make_table(
+        first_name="Sam",
+        last_name="Redmond",
+        shirt_color="pink"
+    )
+
+    make_table(
+        key_justify="right",
+        value_justify="center",
+        song="Style",
+        artist_fullname="Taylor $wift",
+        album="1989"
+    )
+
+test_make_table()
+
+#Function Nuances
+
+def say_hello():
+    print("Hello!")
+
+print(say_hello())  # => ?
+
+def echo(arg=None):
+    print("arg:", arg)
+    return arg
+
+print(echo())  # => ?
+print(echo(5)) # => ?
+print(echo("Hello")) # => ?
+
+def drive(has_car):
+    if not has_car:
+        return
+    return 100  # miles
+
+drive(False)  # => ?
+drive(True)   # => ?
+
+#Parameters and Object Reference
+
+def reassign(arr):
+    arr = [4, 1]
+    print("Inside reassign: arr = {}".format(arr))
+
+def append_one(arr):
+    arr.append(1)
+    print("Inside append_one: arr = {}".format(arr))
+
+l = [4]
+print("Before reassign: arr={}".format(l))  # => ?
+reassign(l)
+print("After reassign: arr={}".format(l))  # => ?
+
+l = [4]
+print("Before append_one: arr={}".format(l))  # => ?
+append_one(l)
+print("After append_one: arr={}".format(l))  # => ?
+
+#Scope
+
+# Case 1
+x = 10
+
+def foo():
+    print("(inside foo) x:", x)
+    y = 5
+    print('value:', x * y)
+
+print("(outside foo) x:", x)
+foo()
+print("(after foo) x:", x)
+
+
+# Case 2
+x = 10
+
+def foo():
+    x = 8  # Only added this line - everything else is the same
+    print("(inside foo) x:", x)
+    y = 5
+    print('value:', x * y)
+
+print("(outside foo) x:", x)
+foo()
+print("(after foo) x:", x)
 
