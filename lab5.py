64d57284da4dc54a7d81671056a56d86787d499a
wgalka <37069490+wgalka@users.noreply.github.com>
2018-12-04 21:59:44 +0100

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,1 +1,24 @@
+class Course:
+    def __init__(self, department, number, title):
+        self.department = department
+        self.number = number
+        self.title = title
 
+class CSCourse(Course):
+    def __init__(self, department, number, title, recorded=False):
+        super().__init__(department, number, title)
+        self.is_recorded = recorded
+                
+type(a)
+    #<class 'courses.Course'>
+isinstance(a, Course)
+    #True
+isinstance(b, Course)
+    #True
+type(a) == type(b)
+    #False
+a == b
+    #False
+  
+  
+        
